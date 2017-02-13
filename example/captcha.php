5dc370df1d2e80fef7b3e58261e41d089f1dfbf2
Rafael <rafaeldecruzeiro@gmail.com>
2017-02-13 13:37:14 -0200

    token

--- example/captcha.php
+++ example/captcha.php
@@ -1,9 +1,8 @@
 <?php 
 require_once("../DecifraCaptcha.php");
 
-$token = "d41d8cd98f00b204e9800998ecf8427e";
-//$token = "4431f462d526d9ceaae1e377f90e3255";
-//$token = 'coloqueseueutokenaqui'; //aqui vai seu token
+
+$token = 'coloqueseueutokenaqui'; //aqui vai seu token
 $arquivo = "../captchas/captcha.jpg"; //aqui vai o seu arquivo com o caminho correspondente
 
 
